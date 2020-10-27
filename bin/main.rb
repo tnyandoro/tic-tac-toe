5a3ab9ee8670e01a37aee72440b08fbce2a51eed
Tendai Nyandoro <tnyandoro@gmail.com>
2020-10-27 15:45:09 +0200

    Define player moves

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 # Player Class
 class Player
   def initialize
@@ -17,7 +18,10 @@ end
 class Main
   def initialize
     @player = Player.new
-    @board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    @board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] # default board
+    @gameover = false
+    @current_player = 0
+    @number_of_moves = 1
   end
 
   def user_information
@@ -25,7 +29,7 @@ class Main
     until @player.getplayers.length == 2
       num = 'first'
       num = 'second' if i == 2
-      puts "Please Enter the #{num} player"
+      print "Please Enter the name of the #{num} player:"
       input = gets.chomp
       @player.add_player({ 'name' => input })
       i += 1
@@ -42,10 +46,33 @@ class Main
     puts
   end
 
+  def correct_move(value)
+    return false unless value.class == Interger
+    return false unless (1..9).to_a.include?(value)
+     true
+  end
+  
+  def play
+     until @gameover 
+      puts @number_of_moves
+      print "#{@player.getplayers[@current_player]['name']} Enter the number : "
+      move = gets.chomp 
+      if correct_move()
+         
+      display_board
+      @current_player = @current_player.zero? ? 1 : 0
+      @number_of_moves += 1
+      @gameover = true if @number_of_moves > 9
+      else
+        puts "wrong input" 
+      end
+     end  
+  end
   def start
     # Get User Information
     user_information
     display_board
+    play
   end
 end
 

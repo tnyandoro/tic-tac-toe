ab036283a409e040412a06e6d16c3d73548276a4
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2020-10-27 14:22:25 +0200

    Merge Rabase 2

--- bin/main.rb
+++ bin/main.rb
@@ -12,3 +12,31 @@ class Player
     @players
   end
 end
+
+# Main Class
+class Main
+  def initialize
+    @player = Player.new
+    @board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+  end
+
+  def user_information
+    i = 1
+    until @player.getplayers.length == 2
+      num = 'first'
+      num = 'second' if i == 2
+      puts "Please Enter the #{num} player"
+      input = gets.chomp
+      @player.add_player({ 'name' => input })
+      i += 1
+    end
+  end
+
+  def start
+    # Get User Information
+    user_information
+    display_board
+  end
+end
+
+Main.new.start
